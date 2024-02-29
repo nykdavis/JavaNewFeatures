756389db5205e51ac28eaa3a89f1e81b2b8ad05a
dvsnayak <yello.davis@gmail.com>
2024-02-29 20:39:03 +0530

    Code updated

--- JavaNewFeatures/src/com/davis/piersqure/java8/stream/MapPractice.java
+++ JavaNewFeatures/src/com/davis/piersqure/java8/stream/MapPractice.java
@@ -16,8 +16,10 @@ public class MapPractice {
 		// Problem 2: Given a list of integers, find the square of each integer in the
 		// list.
 		List<Integer> list1 = Arrays.asList(3, 2, 5, 33, 44, 23, 41, 87, 7, 1);
-		System.out.println(list1.stream().map(i -> i * i).collect(Collectors.toList()));
-
+		//System.out.println(list1.stream().map(i -> i * i).collect(Collectors.toList()));
+		list1.stream().map(i -> i * i).forEach(value -> System.out.print(value+" "));
+		
+		System.out.println("\n");
 		// Problem 3:Given a list of strings, convert each string to uppercase.
 		System.out.println(list.stream().map(i -> i.toUpperCase()).collect(Collectors.toList()));
 
